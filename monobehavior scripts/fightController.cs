cd9e2810dae4f7eb122b7762a136c30b7a713dfc
natalietrevarrow <159745514+natalietrevarrow@users.noreply.github.com>
2024-03-27 02:02:40 -0500

    Update fightController.cs

--- monobehavior scripts/fightController.cs
+++ monobehavior scripts/fightController.cs
@@ -14,9 +14,41 @@ public class fightController : MonoBehaviour
         this.hero_hp_TMP.text = "Boo!!!";
     }
 
+    protected void attackTurn
+    {
+        protected bool currentTurn = true;
+        while(playerHP & monsterHP != 0)
+        {
+            if(currentTurn == true )
+            {
+                int random = Range(1, 20);
+                monsterHP = monsterHP - random;
+                currentTurn = false;
+            }
+            else if(currentTurn == false)
+            {
+                int random = Range(1, 20);
+                if(random >= armorClass)
+                {
+                    playerHP = playerHP - random;
+                    currentTurn = true;
+                }
+            }
+        }
+        if(playerHP == 0)
+        {
+            print("You died!")
+        }
+        else if(monsterHP == 0)
+        {
+            print("You win!")
+        }
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+   =
     }
 }
